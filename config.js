3bc397675ed93b80cb591f0125987da0907bf66a
qanl <30888628+qanl@users.noreply.github.com>
2022-06-22 17:01:57 -0700

    Update config.js

--- config.js
+++ config.js
@@ -28,5 +28,5 @@ export const baseUrl = () => {
 
 // export const username = 'r@gmail.com';
 // export const password = 'Ce9!';
-export const username = 'nlapuste@36eighttechnologies.com';
-export const password = 'Biserica89!';
+export const username = '=';
+export const password = 'ae';
